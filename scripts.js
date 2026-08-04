16e53537b2fb4b90ebe01df7b1096abcbd3823d3
frodoy07 <frodoy07@gmail.com>
2026-08-04 14:51:47 +0100

    * Add new book button * Improve library UI layout * Implement JavaScript functionality for all buttons

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@
 
 const myLibrary = [];
 const container = document.querySelector(".container");
+const addBook = document.querySelector("#addBook");
+const modal = document.querySelector(".modal");
+const submitBook = document.querySelector("#submitBook");
+const radio = document.querySelector("#read");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
 
 const Book = function (title, author, pages, isRead = false, id) {
   this.title = title;
@@ -33,23 +40,48 @@ const displayLibrary = function () {
     container.insertAdjacentHTML(
       "beforeend",
       `
-            <div class="card">
-                  <div>
-                        <h3>Title:</h3>
-                        <p>${el.title}</p>
-                  </div>
-                  <div>
-                        <h3>Author:</h3>
-                        <p>${el.author}</p>
-                  </div>
-                  <div>
-                        <h3>Pages:</h3>
-                        <p>${el.pages}</p>
-                  </div>
-            </div>
+        <div class="card">
+              <div>
+                    <h3>Title:</h3>
+                    <p>${el.title}</p>
+              </div>
+              <div>
+                    <h3>Author:</h3>
+                    <p>${el.author}</p>
+              </div>
+              <div>
+                    <h3>Pages:</h3>
+                    <p>${el.pages}</p>
+              </div>
+        </div>
       `,
     );
   });
 };
 
-displayLibrary();
+// -------- Event Listeners ---------
+radio.addEventListener("click", (event) => {
+  if (event.target.dataset.wasChecked === "true") {
+    event.target.checked = false;
+    event.target.dataset.wasChecked = "false";
+  } else {
+    event.target.checked = true;
+    event.target.dataset.wasChecked = "true";
+  }
+});
+addBook.addEventListener("click", function () {
+  modal.classList.remove("hidden");
+});
+submitBook.addEventListener("click", function (e) {
+  e.preventDefault();
+  const currentTitle = title.value;
+  const currentAuthor = author.value;
+  const currentPages = pages.value;
+  const isRead = radio.checked;
+
+  console.log(currentTitle, currentAuthor, currentPages, isRead);
+  addBookToLibrary(title.value, author.value, pages.value, radio.checked);
+  console.log(myLibrary);
+  modal.classList.add("hidden");
+  displayLibrary();
+});
