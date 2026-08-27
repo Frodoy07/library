b894388f94618d96ab98c3ff6fb5a2c65cb7d087
frodoy07 <frodoy07@gmail.com>
2026-08-27 15:09:34 +0100

    - Refactor toggleBtn and reset() form code verbose

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ const myLibrary = [];
 const container = document.querySelector(".container");
 const addBook = document.querySelector("#addBook");
 const modal = document.querySelector(".modal");
-// const submitBook = document.querySelector("#submitBook");
 const radio = document.querySelector("#read");
 const form = document.querySelector("form");
 const title = document.querySelector("#title");
@@ -13,25 +12,23 @@ const pages = document.querySelector("#pages");
 const close_btn = document.querySelector(".close-btn");
 
 // Functions:
-const Book = function (title, author, pages, isRead = false, id) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-  this.id = id;
-};
+class Book {
+  constructor(title, author, pages, isRead = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this._id = crypto.randomUUID();
+  }
 
-const addBookToLibrary = function (title, author, pages, isRead) {
-  const id = crypto.randomUUID();
-  const newBook = new Book(title, author, pages, isRead, id);
+  addToLibrary() {
+    myLibrary.push(this);
+  }
+}
 
-  myLibrary.push(newBook);
-};
-
-const newBookCard = (book, replace = false) => {
-  const div = !replace
-    ? `
-      <div class='card' data-id='${book.id}'>
+const newBookCard = (book) => {
+  const div = `
+      <div class='card' data-id='${book._id}'>
               <div>
                     <h3>Title:</h3>
                     <p>${book.title}</p>
@@ -44,7 +41,7 @@ const newBookCard = (book, replace = false) => {
                     <h3>Pages:</h3>
                     <p>${book.pages}</p>  
               </div>
-              <div>
+              <div class="read-status">
                 <h3>Read:</h3>
                 <p>${book.isRead ? "✔️" : "❌"}</p>
               </div>
@@ -53,28 +50,7 @@ const newBookCard = (book, replace = false) => {
                 <button type="button" class="delete-btn"><i class="fa-solid fa-trash-can"></i></button>
               </div>
         </div>
-    `.trim()
-    : `
-       <div>
-                    <h3>Title:</h3>
-                    <p>${book.title}</p>
-              </div>
-              <div>
-                    <h3>Author:</h3>
-                    <p>${book.author}</p>
-              </div>
-              <div>
-                    <h3>Pages:</h3>
-                    <p>${book.pages}</p>  
-              </div>
-              <div>
-                <h3>Read:</h3>
-                <p>${book.isRead ? "✔️" : "❌"}</p>
-              </div>
-              <div>
-                <button type="button" class="read-toggle-btn"><i class="fa-solid fa-book-open"></i></button>
-                <button type="button" class="delete-btn"><i class="fa-solid fa-trash-can"></i></button>
-              </div>`.trim();
+    `.trim();
 
   return div;
 };
@@ -89,42 +65,40 @@ addBook.addEventListener("click", function () {
 });
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  // const currentTitle = title.value;
-  // const currentAuthor = author.value;
-  // const currentPages = pages.value;
-  // const isRead = radio.checked;
 
-  addBookToLibrary(title.value, author.value, pages.value, radio.checked);
+  const newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    radio.checked,
+  );
+  newBook.addToLibrary();
   modal.classList.add("hidden");
   displayBook(myLibrary.at(-1));
 
-  title.value = "";
-  author.value = "";
-  pages.value = "";
-  radio.checked = false;
+  form.reset();
 });
 
 container.addEventListener("click", (e) => {
+  const card = e.target.closest(".card");
   const deleteBook = e.target.closest(".delete-btn");
   const readToggleBtn = e.target.closest(".read-toggle-btn");
-  const card = e.target.closest(".card");
+  const readStatus = card.querySelector(".read-status p");
+
   if (!card) return;
-  const nodeList = document.getElementsByClassName("card");
-  const index = myLibrary.findIndex((book) => book.id === card.dataset.id);
+  const index = myLibrary.findIndex((book) => book._id === card.dataset.id);
 
   if (deleteBook) {
     if (index !== -1) {
+      console.log("clicked");
       myLibrary.splice(index, 1);
       card.remove();
     }
   } else if (readToggleBtn) {
+    console.log("toggle clicked");
     if (index !== -1) {
       myLibrary[index].isRead = !myLibrary[index].isRead;
-      const newDiv = document.createElement("div");
-      newDiv.dataset.id = myLibrary[index].id;
-      newDiv.classList.add("card");
-      newDiv.innerHTML = newBookCard(myLibrary[index], true);
-      nodeList[index].replaceWith(newDiv);
+      readStatus.textContent = `${myLibrary[index].isRead ? "✔️" : "❌"}`;
     }
   }
 });
